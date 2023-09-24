4b5fe52dd2a498865d327632f2f212f9db2adb7a
ParasRaina01 <90261069+ParasRaina01@users.noreply.github.com>
2023-09-24 13:11:11 +0530

    dependency issue fixed

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+export const InitialModal = () => {
+  const form = useForm({
+
+  })
+  return (
+    <Dialog open>
+      <DialogContent
+        className="bg-white text-black p-0
+            overflow-hidden"
+      >
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later. 
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
