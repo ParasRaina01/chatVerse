f0306a6e4d72f70b3862782808aad6e69c7b4dab
Paras Raina <parasraina.33@gmail.com>
2023-10-18 01:32:41 +0530

    test

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,12 +4,13 @@ import { useState,useEffect } from "react";
 // import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-server-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-// import { MembersModal } from "@/components/modals/members-modal";
-// import { CreateChannelModal } from "@/components/modals/create-channel-modal";
-// import { LeaveServerModal } from "@/components/modals/leave-server-modal";
-// import { DeleteServerModal } from "@/components/modals/delete-server-modal";
-// import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
-// import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 // import { MessageFileModal } from "@/components/modals/message-file-modal";
 // import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
@@ -28,6 +29,15 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal/>
+      <EditServerModal/>
+      <MembersModal/>
+      <CreateChannelModal/>
+      <LeaveServerModal/>
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      {/* <MessageFileModal /> */}
+      {/* <DeleteMessageModal /> */}
     </>
   )
 }
